abc81b74fb530ebb9235014694ce46fe31b593a2
GuiScarparo <guilhermescarparogrande@gmail.com>
2025-08-14 10:32:51 -0300

    first commit

---
+++ operadores.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 10;
+    $b = 5;
+    echo $a + $b. "<br>";
+    echo "soma: ".$a. "+". $b. "=" . $a+$b."<br>";
+    echo "subtração". $a. "-". $b. "=". $a-$b."<br>";
+    echo "multiplicação". $a. "x". $b. "=". $a*$b."<br>";
+    echo "divisão". $a. "/". $b. "=". $a/$b."<br>";
+    ?>
---
+++ resto.php
@@ -0,0 +1,7 @@
+<?php
+    $a = 10;
+    $b = 3;
+    $resto = $a % $b;
+    echo "conta ".$a. "/". $b. "<br>";
+    echo "resultado ". $resto."<br>"; 
+    ?>
